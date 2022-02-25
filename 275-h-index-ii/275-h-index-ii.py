b0b1a763fd20194795e037e95feeefa36914a7e5
Kaleb Mathewos <94765666+McKaleb@users.noreply.github.com>
2022-02-25 21:58:45 +0300

    Time: 157 ms (69.42%), Space: 20.7 MB (65.97%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.py
+++ 275-h-index-ii/275-h-index-ii.py
@@ -1,36 +1,16 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        x = len(citations)
-        hindex = 0 
-        if citations[0]>=x:
-            return x
-        left = 0
-        right = x-1
-        end,key = 0,0
-        istrue = True
-        while left <= right:
-            
-            mid = left + (right-left)//2
-            leng = len(citations[mid:])
-            if leng >= citations[mid]:
-                hindex = citations[mid]
-                try:
-                    end = citations[mid+1]
-                    key = mid+1
-                except IndexError:
-                    istrue = False
-                left = mid+1
+        left, right = 0, len(citations)-1
+        best = -1
+
+        while left<=right:
+            mid = (left+right)//2
+
+            if (len(citations)-mid) <= citations[mid]:
+                best = mid
+                right = mid-1
+
             else:
-                right=mid-1
-        ans = hindex
-        z = leng = len(citations[key:])
-        if citations[key-1] != citations[key] and istrue:
-            while hindex <= end:
-                mid = hindex+(end-hindex)//2
-                if mid <= z:
-                    ans = mid
-                    hindex = mid+1
-                else:
-                    end = mid-1
-        return ans
-        print(hindex,end,z)
+                left = mid+1
+
+        return len(citations)-best if best!=-1 else 0
