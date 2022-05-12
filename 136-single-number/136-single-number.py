e28e06ef4049f988fbd688ec23fc3d3baaa598ab
Kaleb Mathewos <94765666+McKaleb@users.noreply.github.com>
2022-05-12 18:04:22 +0300

    Time: 228 ms (31.66%), Space: 16.9 MB (21.37%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,7 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        nums = sorted(nums)
-        for i in range(0, len(nums)-1, 2):
-            if nums[i] != nums[i+1]:
-                return nums[i]
-        return nums[-1]
+        dict_list = {}
+        for i in nums:
+            if i in dict_list:
+                dict_list[i] += 1
+            else:
+                dict_list[i] = 1
+        key_list = list(dict_list.keys())
+        value_list = list(dict_list.values())
+        position = value_list.index(min(dict_list.values()))
+        return key_list[position]
