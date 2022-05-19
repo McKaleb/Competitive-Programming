cc93e6c9473409e9576f16e32b888ea7d7868721
Kaleb Mathewos <94765666+McKaleb@users.noreply.github.com>
2022-05-19 19:06:37 +0300

    Time: 221 ms (90.50%), Space: 25.4 MB (30.16%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,12 +1,4 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        rot = 0
-        for i in range(k):
-            rot = nums[-1]
-            nums.pop()
-            nums.insert(0, rot)
-            
-        
+        k %= len(nums)
+        nums[:] = nums[len(nums) - k :] + nums[: len(nums) - k]
